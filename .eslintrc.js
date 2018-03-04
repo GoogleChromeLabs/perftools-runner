37b616888474bf11cb9fdcdc62d271f6e4262054
Eric Bidelman <ebidel@gmail.com>
2018-03-03 22:09:49 -0800

    remove lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,9 +58,9 @@ module.exports = {
 
     // Disabled rules
     // "require-jsdoc": 0,
-    // "valid-jsdoc": 0,
+    "valid-jsdoc": 0,
     // "comma-dangle": 0,
-    // "arrow-parens": 0,
-    // "no-console": 0
+    "arrow-parens": 0,
+    "no-console": 0
   }
 }
